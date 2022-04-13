3d90d22c030ed19155bf76389a708df5d1c75aea
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-04-13 14:31:52 +0700

    adding routes

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Routes, Route, Link } from "react-router-dom";
+import Testing from "./page/Testing";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+          {/* <Route path="about" element={<About />} /> */}
+          <Route path="testing" element={<Testing />} />
+        </Routes>
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
---
+++ src/frontend/src/page/Testing.jsx
@@ -0,0 +1,11 @@
+export const Testing = () => {
+  return (
+    <div>
+      {/* buat coba2 component */}
+      <h1>testing</h1>
+      {/* buat coba2 component */}
+    </div>
+  );
+};
+
+export default Testing;
