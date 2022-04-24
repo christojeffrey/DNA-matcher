6feb228f827dfa0c832ba958cc88e0bdde50a0f3
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-04-24 19:24:22 +0700

    blom bersihin spasi, formating tanggal ke format yg bisa dimengerti sql, rusak kalo ada penyakit yg namanya kek nama bulan

--- src/backend/algo_parseSearch.go
+++ src/backend/algo_parseSearch.go
@@ -11,10 +11,29 @@ func parseSearch(text string) (string, string) {
 	fmt.Println(text)
 	// var regextanggal, _ = regexp.Compile(`\d{2}\-\d{2}\-\d{4}`)
 	var regextanggalkata, _ = regexp.Compile(`(?i)\d{2} (januari|februari|Maret|April|mei|juni|july|agustus|september|oktober|november|desember|january|february|march|may|june|july|august|october|december) \d{4}`)
-	var regexpenyakit, _ = regexp.Compile(`[^(0-9)]+`)
+	var regexpenyakitkotor, _ = regexp.Compile(`[^(0-9)]+`)
+	// var regexpenyakit, _ = regexp.Compile(`^(?:(?!januari|februari).)*$`)
+	var regexinvalidpenyakit, _ = regexp.Compile(`(?i)(januari|februari|Maret|April|mei|juni|july|agustus|september|oktober|november|desember|january|february|march|may|june|july|august|october|december)`)
 	var tanggal = regextanggalkata.FindString(text)
-	var penyakit = regexpenyakit.FindAllString(text,-1)
+	var penyakits = regexpenyakitkotor.FindAllString(text,-1)
+
+	//array of valid penyakit
+	var validPenyakit []string
+	//for earch penyakits
+	for _, penyakit := range penyakits {
+		fmt.Println(penyakit)
+		// if penyakit is valid
+		if !regexinvalidpenyakit.MatchString(penyakit) {
+			validPenyakit = append(validPenyakit, penyakit)
+		}
+
+	}
+	fmt.Println("validpenyakit:")
+	for _, penyakit := range validPenyakit {
+		fmt.Println(penyakit)
+	}
+	
 	fmt.Println("tanggal:", tanggal)
-	fmt.Println("penyakit:", penyakit)
+	fmt.Println("penyakit:", validPenyakit)
 	return tanggal, "penyakit"
 }
