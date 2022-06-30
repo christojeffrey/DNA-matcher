261d70746d9e98db8a9b082300272e7aca3b8c08
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2022-06-30 10:53:08 +0700

    create front end branch

--- src/backend/config.go
+++
@@ -1,3 +0,0 @@
-package main
-
-var databaseReference = "root@tcp(localhost:3306)/"
---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const BE_URL = "https://deoxyde.herokuapp.com/";
+// export const BE_URL = "http://localhost:1323/";
